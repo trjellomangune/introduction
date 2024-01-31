632c990578edcf393c49e841aa70df29077f4830
trjellomangune <jello.mangune@tooltwist.com>
2024-01-31 17:18:11 +0800

    Added Expectations and Impressions to TwistResources

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,12 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello! I am Jello. You can call me Jel in short and I am an aspiring software developer.</p>
+        <p>My expectations to TwistResources is that it is a company that will aide my learning and career growth.
+          I am expecting to learn a lot not only in the aspect of coding but with the whole process of the software development.
+        </p>
+        <p>My impression so far for TwistResources is that it have a very nice environment full of nice people. 
+          As for the learning path scheduled for the upcoming days, it piqued my interest and I am very excited to have the lessons.
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
